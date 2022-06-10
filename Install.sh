f4efb8fac6e839b215e9facf4102ff807784bc51
Martin Bies <MBies87@googlemail.com>
2022-06-10 09:22:06 +0200

    Update install script

--- Install.sh
+++ Install.sh
@@ -31,6 +31,22 @@ echo ""
 
 sudo apt-get install htop autoconf build-essential libgmp-dev libtool git libatlas-base-dev libblas-dev liblapack-dev screen libcdd-dev 4ti2 singular
 
+# For NormalizInterface, we have to configure gap with the gmp installed on the system. Here, I try this by installing limgmp-dev above and then configuring gap via:
+# ./configure --with-gmp=system
+# This option "system" is by now deprecated, but still seems functional. Should this not be the case, please try a local installation of gmp and then link gap to it. This
+# should look roughly as follows:
+
+#wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
+#tar xvf gmp-6.1.2.tar.xz
+#cd gmp-6.1.2
+#./configure --prefix=/usr/local/gmp/6_1_2
+#make
+#make check
+#sudo make install
+# Now navigate into your gap folder (below this is the folder gap-4-11.1) and execute:
+#./configure --with-gmp=/usr/local/gmp/6_1_2
+
+
 echo ""
 echo ""
 echo "------------------------------------------------------------------------"
@@ -119,6 +135,9 @@ echo ""
 echo "(*) Clone NConvex"
 git clone https://github.com/homalg-project/NConvex.git
 echo ""
+echo "(*) Clone NormalizInterface"
+git clone https://github.com/gap-packages/NormalizInterface.git
+echo ""
 echo "(*) Clone PrimaryDecomposition"
 git clone https://github.com/homalg-project/PrimaryDecomposition.git
 echo ""
@@ -157,7 +176,17 @@ cd CddInterface/
 cd ..
 
 echo ""
-echo "(*) Install and test ToricVarieties_project"
+echo "(*) Install NormalizInterface"
+echo ""
+cd NormalizInterface/
+./build-normaliz.sh ../../..
+./autogen.sh
+./configure --with-gaproot=../../..
+make
+cd ..
+
+echo ""
+echo "(*) Install ToricVarieties_project"
 echo ""
 cd ToricVarieties_project/
 make install
